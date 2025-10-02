390978e91044a33ed218ea60a58daf193e0e0df8
damondel <80132267+damondel@users.noreply.github.com>
2025-10-02 15:45:32 -0700

    Updated tsconfig.json

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: './web',
+  root: '.',
   publicDir: 'public',
   server: {
     port: 5173
   },
   build: {
-    outDir: '../dist'
+    outDir: 'dist'
   }
 })
